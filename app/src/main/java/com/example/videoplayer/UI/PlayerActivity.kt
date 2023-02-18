5ddb62bdeb869e39101054c7ba673d5ffff8433e
Mohit Singh <singhmohit9718@gmail.com>
2023-02-18 23:58:27 +0530

    succes fully added the exo player
    Signed-off-by: Mohit Singh <singhmohit9718@gmail.com>

---
+++ app/src/main/java/com/example/videoplayer/UI/PlayerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.videoplayer.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+    }
+}
